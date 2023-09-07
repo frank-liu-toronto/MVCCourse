550f378def9e812c072d8e97855bb761665ec8e0
Frank Liu <fliu@proserveit.com>
2023-09-07 14:37:58 -0400

    4. Add Support for MVC framework

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -2,77 +2,17 @@ using System.Net.Mime;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", (HttpContext context) =>
-{
-    WriteHtml(context, @$"
-            <!doctype html>
-            <html>
-              <head><title>miniHTML</title></head>
-              <body>
-                <h1>Simple Framework</h1>
-                <br/>
-                <form action=""/login"" method=""post"">
-                  <label for=""username"">User name:</label>
-                  <input type=""text"" id=""username"" name=""username"" required>
-                  <label for=""password"">Password:</label>
-                  <input type=""password"" id=""password"" name=""password"" required>
-                  <button type=""submit"">Login</button>
-                </form>
-              </body>
-            </html>");
-});
+app.UseRouting();
 
-app.MapPost("/login", (HttpContext context) =>
-{
-    var username = context.Request.Form["username"];
-    var password = context.Request.Form["password"];
-
-    if (username == "frank" && password == "password")
-    {
-        var html = @$"
-            <!doctype html>
-            <html>
-              <head><title>miniHTML</title></head>
-              <body>
-                <h1>Simple Framework</h1>
-                <br/>
-                Welcome to our simple framework!
-              </body>
-            </html>";
-        WriteHtml(context, html);
-    }
-    else
-    {
-        var html = @$"
-            <!doctype html>
-            <html>
-              <head><title>miniHTML</title></head>
-              <body>
-                <h1>Simple Framework</h1>
-                <br/>
-                <form action=""/login"" method=""post"">
-                  <label for=""username"">User name:</label>
-                  <input type=""text"" id=""username"" name=""username"" required>
-                  <label for=""password"">Password:</label>
-                  <input type=""password"" id=""password"" name=""password"" required>
-                  <button type=""submit"">Login</button>
-                  <br/>
-                  <label style=""color:red"">Login failed!</label>
-                </form>
-              </body>
-            </html>";
-        WriteHtml(context, html);
-    }
-});
-
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
-void WriteHtml(HttpContext context, string html)
-{
-    context.Response.ContentType = MediaTypeNames.Text.Html;
-    context.Response.ContentLength = Encoding.UTF8.GetByteCount(html);
-    context.Response.WriteAsync(html);
-}
+
