224ac496af04e3f45547b47485207bd79648e969
Frank Liu <fliu@proserveit.com>
2023-09-07 14:39:08 -0400

    5. Handle requests with Controllers

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World from Action Method.";
+        }
+
+        public string Error()
+        {
+            return "I have an error here.";
+        }
+    }
+}
