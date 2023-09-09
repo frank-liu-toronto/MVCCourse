5cb23d5eb86f1296cd9c696b324825e4f99e95d7
Frank Liu <fliu@proserveit.com>
2023-09-09 12:40:06 -0400

    13. Use Static Repository to Store Data

---
+++ WebApp/Models/CategoriesRepository.cs
@@ -0,0 +1,59 @@
+﻿namespace WebApp.Models
+{
+    public static class CategoriesRepository
+    {
+        private static List<Category> _categories = new List<Category>()
+        {
+            new Category { CategoryId = 1, Name = "Beverage", Description = "Beverage" },
+            new Category { CategoryId = 2, Name = "Bakery", Description = "Bakery" },
+            new Category { CategoryId = 3, Name = "Meat", Description = "Meat" }
+        };
+
+        public static void AddCategory(Category category)
+        {
+            var maxId = _categories.Max(x => x.CategoryId);
+            category.CategoryId = maxId + 1;
+            _categories.Add(category);
+        }
+
+        public static List<Category> GetCategories() => _categories;
+
+        public static Category? GetCategoryById(int categoryId)
+        {
+            var category = _categories.FirstOrDefault(x => x.CategoryId == categoryId);
+            if (category != null) 
+            { 
+                return new Category
+                {
+                    CategoryId = category.CategoryId,
+                    Name = category.Name,
+                    Description = category.Description,                    
+                };
+            }
+
+            return null;
+        }
+
+        public static void UpdateCategory(int categoryId, Category category)
+        {
+            if (categoryId != category.CategoryId) return;
+
+            var categoryToUpdate = GetCategoryById(categoryId);
+            if (categoryToUpdate != null)
+            {
+                categoryToUpdate.Name = category.Name;
+                categoryToUpdate.Description = category.Description;
+            }
+        }
+
+        public static void DeleteCategory(int categoryId)
+        {
+            var category = _categories.FirstOrDefault(x => x.CategoryId == categoryId);
+            if (category != null)
+            {
+                _categories.Remove(category);
+            }
+        }
+
+    }
+}
