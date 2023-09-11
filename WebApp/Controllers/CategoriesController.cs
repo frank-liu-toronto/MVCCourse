88fb47e88c57321b75949a280ac6adbeb5067317
Frank Liu <fliu@proserveit.com>
2023-09-10 20:06:57 -0400

    17. Understand Model Binding in HttpGet action method

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -13,7 +13,7 @@ namespace WebApp.Controllers
 
         public IActionResult Edit(int? id) 
         {
-            var category = new Category { CategoryId = id.HasValue?id.Value:0 };
+            var category = CategoriesRepository.GetCategoryById(id.HasValue?id.Value:0);
 
             return View(category);
         }
