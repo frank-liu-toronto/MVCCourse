59037ae52a6fc7f345df12614a0ef1d6265d6b15
Frank Liu <fliu@proserveit.com>
2023-09-15 18:16:20 +0800

    20. Use Hidden field to store Id in the form

--- WebApp/Models/CategoriesRepository.cs
+++ WebApp/Models/CategoriesRepository.cs
@@ -38,7 +38,7 @@
         {
             if (categoryId != category.CategoryId) return;
 
-            var categoryToUpdate = GetCategoryById(categoryId);
+            var categoryToUpdate = _categories.FirstOrDefault(x => x.CategoryId == categoryId);
             if (categoryToUpdate != null)
             {
                 categoryToUpdate.Name = category.Name;
