3995807a253d57d6b61c423e03b717a5cb07c5d7
Frank Liu <fliu@proserveit.com>
2023-09-15 18:04:44 +0800

    19. Handle HttpPost request

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -18,5 +18,12 @@ namespace WebApp.Controllers
             return View(category);
         }
 
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            CategoriesRepository.UpdateCategory(category.CategoryId, category);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
