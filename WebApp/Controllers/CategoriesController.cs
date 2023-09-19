2cc3b77d4f2c4048d6e164a46ea02fb1191b79af
Frank Liu <fliu@proserveit.com>
2023-09-19 17:56:09 +0800

    23. Assignment 1: Answer

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -30,5 +30,22 @@ namespace WebApp.Controllers
             return View(category);
         }
 
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.AddCategory(category);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
     }
 }
--- WebApp/Models/Category.cs
+++ WebApp/Models/Category.cs
@@ -8,6 +8,6 @@ namespace WebApp.Models
 
         [Required]
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
     }
 }
