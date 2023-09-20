24d770ea64f4743914ecd786d11b1051a5a31f27
Frank Liu <fliu@proserveit.com>
2023-09-20 20:47:32 +0800

    26. Course Project Requirements & Prepared Source Code

---
+++ WebApp/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int? CategoryId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public int? Quantity { get; set; }
+
+        [Required]
+        public double? Price { get; set; }
+        
+    }
+}
---
+++ WebApp/Models/ProductsRepository.cs
@@ -0,0 +1,63 @@
+﻿namespace WebApp.Models
+{
+    public class ProductsRepository
+    {
+        private static List<Product> _products = new List<Product>()
+        {
+            new Product { ProductId = 1, CategoryId = 1, Name = "Iced Tea", Quantity = 100, Price = 1.99 },
+            new Product { ProductId = 2, CategoryId = 1, Name = "Canada Dry", Quantity = 200, Price = 1.99 },
+            new Product { ProductId = 3, CategoryId = 2, Name = "Whole Wheat Bread", Quantity = 300, Price = 1.50 },
+            new Product { ProductId = 4, CategoryId = 2, Name = "White Bread", Quantity = 300, Price = 1.50 }
+        };
+
+        public static void AddProduct(Product product)
+        {
+            var maxId = _products.Max(x => x.ProductId);
+            product.ProductId = maxId + 1;
+            _products.Add(product);
+        }
+
+        public static List<Product> GetProducts() => _products;
+
+        public static Product? GetProductById(int productId)
+        {
+            var product = _products.FirstOrDefault(x => x.ProductId == productId);
+            if (product != null)
+            {
+                return new Product
+                {
+                    ProductId = product.ProductId,
+                    Name = product.Name,
+                    Quantity = product.Quantity,
+                    Price = product.Price,
+                    CategoryId = product.CategoryId                    
+                };
+            }
+
+            return null;
+        }
+
+        public static void UpdateProduct(int productId, Product product)
+        {
+            if (productId != product.ProductId) return;
+
+            var productToUpdate = _products.FirstOrDefault(x => x.ProductId == productId);
+            if (productToUpdate != null)
+            {
+                productToUpdate.Name = product.Name;
+                productToUpdate.Quantity = product.Quantity;
+                productToUpdate.Price = product.Price;
+                productToUpdate.CategoryId = product.CategoryId;
+            }
+        }
+
+        public static void DeleteProduct(int productId)
+        {
+            var product = _products.FirstOrDefault(x => x.ProductId == productId);
+            if (product != null)
+            {
+                _products.Remove(product);
+            }
+        }
+    }
+}
