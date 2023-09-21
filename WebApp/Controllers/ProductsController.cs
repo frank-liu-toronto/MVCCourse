f18311f0bec2b145b560d3ecee0ce38665f2daad
Frank Liu <fliu@proserveit.com>
2023-09-21 22:11:11 +0800

    29. Implement Add Product

--- WebApp/Controllers/ProductsController.cs
+++ WebApp/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
+using WebApp.ViewModels;
 
 namespace WebApp.Controllers
 {
@@ -9,6 +10,29 @@ namespace WebApp.Controllers
         {
             var products = ProductsRepository.GetProducts(loadCategory: true);
             return View(products);
+        }
+
+        public IActionResult Add()
+        {
+            var productViewModel = new ProductViewModel
+            {
+                Categories = CategoriesRepository.GetCategories()
+            };
+
+            return View(productViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(ProductViewModel productViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductsRepository.AddProduct(productViewModel.Product);                
+                return RedirectToAction(nameof(Index));
+            }
+            
+            productViewModel.Categories = CategoriesRepository.GetCategories();
+            return View(productViewModel);
         }
     }
 }
---
+++ WebApp/ViewModels/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WebApp.Models;
+
+namespace WebApp.ViewModels
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Category> Categories { get; set; } = new List<Category>();
+        public Product Product { get; set; } = new Product();
+    }
+}
