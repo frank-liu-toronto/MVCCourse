cbcdda0a8bdc50b75054f808c9eab33c3d515ad3
Frank <liu1000@gmail.com>
2023-10-22 20:08:10 -0400

    44. Custom Validation with ValidationAttribute

--- WebApp/ViewModels/SalesViewModel.cs
+++ WebApp/ViewModels/SalesViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using WebApp.Models;
+using WebApp.ViewModels.Validations;
 
 namespace WebApp.ViewModels
 {
@@ -12,6 +13,7 @@ namespace WebApp.ViewModels
 
         [Display(Name = "Quantity")]
         [Range(1, int.MaxValue)]
+        [SalesViewModel_EnsureProperQuantity]
         public int QuantityToSell { get; set; }
     }
 }
---
+++ WebApp/ViewModels/Validations/SalesViewModel_EnsureProperQuantity.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApp.Models;
+
+namespace WebApp.ViewModels.Validations
+{
+    public class SalesViewModel_EnsureProperQuantity : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var salesViewModel = validationContext.ObjectInstance as SalesViewModel;
+
+            if (salesViewModel != null)
+            {
+                if (salesViewModel.QuantityToSell <= 0)
+                {
+                    return new ValidationResult("The quantity to sell has to be greater than zero.");
+                }
+                else
+                {
+                    var product = ProductsRepository.GetProductById(salesViewModel.SelectedProductId);
+                    if (product != null)
+                    {
+                        if (product.Quantity < salesViewModel.QuantityToSell)
+                            return new ValidationResult($"{product.Name} only has {product.Quantity} left. It is not enough.");
+                    }
+                    else
+                    {
+                        return new ValidationResult("The selected product doesn't exist.");
+                    }
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
