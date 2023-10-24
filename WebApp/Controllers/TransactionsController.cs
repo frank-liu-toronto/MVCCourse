e2b32e3346a4351df2ff1ccab60c23f909bc14af
Frank <liu1000@gmail.com>
2023-10-24 16:11:47 -0400

    49. Assignment 4: Answer

---
+++ WebApp/Controllers/TransactionsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            TransactionsViewModel transactionsViewModel = new TransactionsViewModel();
+            return View(transactionsViewModel);
+        }
+
+        public IActionResult Search(TransactionsViewModel transactionsViewModel)
+        {
+            var transactions = TransactionsRepository.Search(
+                transactionsViewModel.CashierName??string.Empty,
+                transactionsViewModel.StartDate,
+                transactionsViewModel.EndDate);
+
+            transactionsViewModel.Transactions = transactions;
+
+            return View("Index", transactionsViewModel);
+        }
+    }
+}
---
+++ WebApp/ViewModels/TransactionsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApp.Models;
+
+namespace WebApp.ViewModels
+{
+    public class TransactionsViewModel
+    {
+        [Display(Name = "Cashier Name")]
+        public string? CashierName { get; set; }
+
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; } = DateTime.Today;
+
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; } = DateTime.Today;
+
+        public IEnumerable<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+}
