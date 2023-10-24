f51105f168cfaa25bea84146c13b6ccb25900952
Frank <liu1000@gmail.com>
2023-10-23 21:30:12 -0400

    46. What is ViewComponent

---
+++ WebApp/ViewComponents/TransactionsViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.ViewComponents
+{
+    [ViewComponent]
+    public class TransactionsViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+
+            return "List of Transactions";
+        }
+    }
+}
